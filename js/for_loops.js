689bcf8ae1dd96e3f4a0777e45af176caec8d397
Michael Garrison <mgarrison.sa@gmail.com>
2020-04-28 15:02:35 -0500

    loops exercise

---
+++ js/for_loops.js
@@ -0,0 +1,35 @@
+"use strict";
+//For Loops #2
+// function showMultiplicationTable(num) {
+//     for (var i = 1; i <= 10; i++) {
+//         console.log(num + " x " + i + " = " + (num * i));
+//     }
+// }
+//
+// showMultiplicationTable(7)
+
+
+//For Loops #3
+// function evenOrOdd(num) {
+//     return (num % 2 === 0) ? num + " is an even number" : num + " is an odd number";
+// }
+//
+// for (var i = 0; i < 10; i++) {
+//     var randomNumber = Math.floor(Math.random() * 100) + 20;
+//     console.log(evenOrOdd(randomNumber));
+// }
+
+
+//For Loops #4
+// for (var i = 0; i <= 9; i++) {
+//     for (var k = 1; k <= i; k++) {
+//         console.log(i)
+//     }
+//     console.log()
+// }
+
+
+//For Loops #5
+// for (var i = 100; i >=5; i -= 5) {
+//     console.log(i)
+// }
