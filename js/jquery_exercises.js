f7c0fba954db69574d14f4db4ca4e55782a49a7a
Michael Garrison <mgarrison.sa@gmail.com>
2020-05-12 15:15:35 -0500

    js for jquery selectors exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -6,6 +6,21 @@
 //     });
 // })(jQuery);
 
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
+// $(document).ready(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+$(document).ready(function () {
+    var myHeading = $('h1').html();
+    alert(myHeading);
+
+    $('.codeup').css('border', '2px solid red');
+
+    $('li').css('font-size', '20px');
+
+    $('h1,p,li').css('background-color', 'grey');
+
+    $('#container').css('color','white');
+
+    $('#container').css('background-color', 'black');
+
 });
