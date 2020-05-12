7d96163cfeae21956761ad9a460e506d0a552fa5
Michael Garrison <mgarrison.sa@gmail.com>
2020-05-12 11:06:10 -0500

    js file for jquery exercises

---
+++ js/jquery_exercises.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// (function($){
+//     $(document).ready(function() {
+//         alert( 'The DOM has finished loading!' );
+//     });
+// })(jQuery);
+
+$(document).ready(function() {
+    alert( 'The DOM has finished loading!' );
+});
