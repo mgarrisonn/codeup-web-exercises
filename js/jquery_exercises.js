c814cee703600c85507e8a62a92ab0b4b42f47ae
Michael Garrison <mgarrison.sa@gmail.com>
2020-05-13 11:19:09 -0500

    added mouse events to jquery exercises

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -9,18 +9,45 @@
 // $(document).ready(function() {
 //     alert( 'The DOM has finished loading!' );
 // });
-$(document).ready(function () {
-    var myHeading = $('h1').html();
-    alert(myHeading);
+// $(document).ready(function () {
+//     var myHeading = $('h1').html();
+//     alert(myHeading);
+//
+//     $('.codeup').css('border', '2px solid red');
+//
+//     $('li').css('font-size', '20px');
+//
+//     $('h1,p,li').css('background-color', 'grey');
+//
+//     // $('#container').css('color','white');
+//     //
+//     // $('#container').css('background-color', 'black');
+//
+//     $('#container').css( {
+//         'color': 'white',
+//         'background-color': 'black'
+//     });
+// });
 
-    $('.codeup').css('border', '2px solid red');
-
-    $('li').css('font-size', '20px');
-
-    $('h1,p,li').css('background-color', 'grey');
-
-    $('#container').css('color','white');
-
-    $('#container').css('background-color', 'black');
-
+// change background color of h1
+$('h1').click(function(){
+    $(this).css({
+       'background-color': 'grey',
+    });
 });
+
+// change font size of p to 18px
+$('p').dblclick(function () {
+    $(this).css({
+       'font-size': '18px',
+    });
+});
+
+// change text color to red when hover and back to black
+$('li').hover(function () {
+    $(this).css('color', 'red');
+    },
+    function () {
+    $(this).css('color', 'black');
+    }
+);
