2eff3c1a7559f81e73397ad1dcdeaf58ea589eef
Michael Garrison <mgarrison.sa@gmail.com>
2020-05-20 23:15:06 -0500

    updated js/html

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,6 +1,18 @@
 "use strict";
 $(document).ready(function () {
+    mapboxgl.accessToken = mapboxKey;
 
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: 10,
+        center: {lng: -98.4936, lat: 29.4241},
+    });
+    //---------marker----------
+    var marker = new mapboxgl.Marker();
+    marker.setLngLat({lng: -98.4936, lat: 29.4241});
+    marker.addTo(map);
+    marker.setDraggable(true);
     //-----------weather for san antonio--------
     // var weatherIcon = 'http://openweathermap.org/img/wn/10d@2x.png';
     var weatherUrl = 'https://api.openweathermap.org/data/2.5/onecall';
@@ -12,12 +24,13 @@ $(document).ready(function () {
         units: 'imperial',
         timezone: 'Chicago'
     };
+
     //-------------5 day forecast-------------
-    $.get(weatherUrl, weatherOptions).done(function (data) {
-        console.log(data);
-        console.log(data.daily);
-        console.log(data.daily[0]);
-        function weeklyForecast() {
+    function weeklyForecast() {
+        $.get(weatherUrl, weatherOptions).done(function (data) {
+            console.log(data);
+            console.log(data.daily);
+            console.log(data.daily[0]);
             data.daily.forEach(function (day, index) {
                 if (index <= 4) {
                     var grabDate = new Date(day.dt * 1000);
@@ -28,56 +41,46 @@ $(document).ready(function () {
                     html += "<li class='list-group-item'>" + "Humidity: " + day.humidity + "</li>";
                     html += "<li class='list-group-item'>" + "Wind: " + day.wind_speed + "</li>";
                     html += "<li class='list-group-item'>" + "Pressure: " + day.pressure + "</li>";
-
                     html += "</div>";
                     $('#weather-info').append(html);
                 }
             });
-        };
-        //---------weather based on marker-------
-        function markerDaily (){
-            var lngLat = marker.getLngLat();
-            var longitude = lngLat.lng;
-            var latitude = lngLat.lat;
-            weatherOptions.lat = latitude
-            weatherOptions.lon = longitude
-            reverseGeocode({lng: longitude, lat: latitude}, mapboxKey).then(function(result){
-                $('#current-city').empty()
-                $('#current-city').append("Current Location " + result)
-            });
-            weeklyForecast();
-        }
-    });
+        });
+    };
 
+//---------weather based on marker-------
+    function markerDaily() {
+        var lngLat = marker.getLngLat();
+        var longitude = lngLat.lng;
+        var latitude = lngLat.lat;
+        weatherOptions.lat = latitude
+        weatherOptions.lon = longitude
+        reverseGeocode({lng: longitude, lat: latitude}, mapboxKey).then(function (result) {
+            $('#current-city').empty()
+            $('#current-city').append("Current Location " + result)
+        });
+        weeklyForecast();
+    }
+
+
+    //----------search bar-------------
     var searchBar = document.querySelector('#input');
-    $("newLocation").click(function(e) {
+    $("newLocation").click(function (e) {
         e.preventDefault();
-        geocode(searchBar.value, mapboxKey).then(function(result) {
+        geocode(searchBar.value, mapboxKey).then(function (result) {
             map.setCenter(result);
             map.setZoom(10);
             marker.setLngLat(result);
             markerDaily();
-        })
+        });
     });
+    weeklyForecast();
+    marker.on("dragend", markerDaily());
 
-    // MAPBOX
-    mapboxgl.accessToken = mapboxKey;
 
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v9',
-        zoom: 10,
-        center: {lng: -98.4936, lat: 29.4241},
-    });
 
-    var marker = new mapboxgl.Marker();
-    marker.setLngLat({lng: -98.4936, lat: 29.4241});
-    marker.addTo(map);
-    marker.setDraggable(true);
 
-    // reverseGeocode(marker.getLngLat(), mapboxKey).then(function (results) {
-    //     console.log(results);
-    //     $('#current-city').html('Current City: ' + results);
-    // });
+
+
 
 });
