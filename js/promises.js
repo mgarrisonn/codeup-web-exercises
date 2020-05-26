69c3be30907c9f763529a9b98349b0c326a2d5be
michael garrison <mgarrison.sa@gmail.com>
2020-05-26 10:59:10 -0500

    completed exercise

--- js/promises.js
+++ js/promises.js
@@ -1,1 +1,9 @@
 "use strict";
+$(document).ready(function (){
+   function userInfo(username) {
+       var githubAPI = fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `token ${GITHUB_API_KEY}`}});
+       githubAPI.then(response => response.json())
+           .then(events => console.log(new Date(events[0].created_at).toDateString()));
+   }
+   userInfo('mgarrisonn');
+});
